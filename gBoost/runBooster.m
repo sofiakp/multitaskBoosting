9485548ff7307477b8b8f1f7f437e60759281202
Sofia KP <sofiakp@fleas>
2013-12-05 10:58:18 -0800

    Added scripts for extracting dnase at motif hits. runBooster reads dnase signal

--- gBoost/runBooster.m
+++ gBoost/runBooster.m
@@ -1,4 +1,4 @@
-function runBooster(params_file, outfile_root, resume, folds)
+function runBooster(params_file, outfile_root, resume, folds, debug)
 rs = RandStream('mt19937ar', 'Seed', 1);
 RandStream.setGlobalStream(rs);
 
@@ -21,9 +21,11 @@ has_scores = ~isempty(files.score_files{1});
 if has_scores
     load(files.score_files{1});
     sel_mot = strcmp(pssm_source,'hocomoco');
-    scores = scores(sel_mot, :)';
-    mot_names = mot_names(sel_mot);
     nmot = sum(sel_mot);
+    scores = scores(sel_mot, :)'; % now scores is genes x mot
+    scores = max(scores, 0); % Negative scores don't make much sense
+    scores = scores ./ repmat(max(scores, [], 1), ngen, 1); % normalize by the maximum across each motif
+    mot_names = mot_names(sel_mot);
 end
 load(files.fold_file);
 
@@ -36,14 +38,48 @@ for f = 1:length(folds)
   ntasks = size(tasks, 2);
   
   all_ind = find(tr | ts); % The rows of tasks correspond to these indices of cexp
-                           %all_ind = all_ind(1:10:end); % FOR TEST PURPOSES ONLY
-                           %tasks = tasks(1:10:end, :);
+  if debug
+    all_ind = all_ind(1:10:end);
+    tasks = tasks(1:10:end, :);
+  end
   all_tr_ind = ismember(all_ind, find(tr)); % Indicators of rows of tasks that correspond to training examples
   all_ts_ind = ismember(all_ind, find(ts));
   
   [exr exc] = ind2sub(size(tr), all_ind);
   if has_scores
+    if isfield(files, 'mot_activity_list') && ~isempty(files.mot_activity_list) && ...
+        isfield(train_params, 'signal_weight') && train_params.signal_weight > 0
+      % First col is the path to the file with the motif activities, second
+      % col is experiment (cell line).
+      fi = fopen(files.mot_activity_list, 'r');
+      C = textscan(fi, '%s%s');
+      fclose(fi);
+      filenames = C{1};
+      cell_lines = C{2};
+      assert(all(ismember(exptnames, cell_lines)));
+      
+      X = scores(exr, :);
+      
+      for i = 1:nexp
+        sel_file = strcmp(cell_lines, exptnames{i});
+        if ~any(sel_file)
+          error('MyErr:runBooster', ['No signal file provided for ', exptnames{i}]);
+        elseif nnz(sel_file) > 1
+          error('MyErr:runBooster', ['Multiple signal files provided for ', exptnames{i}]);
+        else
+          signal_struct = load(fullfile(indir, filenames{sel_file}));
+          [~, midx] = ismember(mot_names, signal_struct.mot_names);
+          assert(all(midx));
+          signal_struct.signal = signal_struct.signal(midx, :)'; % transpose so that the result is genes x mot
+          sel_ex = exc == i; % get examples corresponding to this experiment
+          sel_gene = exr(sel_ex); % get the gene index of these examples
+          X(sel_ex, :) = X(sel_ex, :) + train_params.signal_weight * signal_struct.signal(sel_gene, :);
+        end
+      end
+      X = [pexp(exc, :) X];
+    else
       X = [pexp(exc, :) scores(exr, :)];
+    end
   else
       X = pexp(exc, :);
   end
